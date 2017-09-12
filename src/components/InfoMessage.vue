5c8e64d04b74c74bd92cd6f0e410ce4c6b957a51
kevinUrbano <kevin_u2004@hotmail.com>
2017-09-13 00:50:16 +0200

    cambio en el diseño del mensaje de errores

--- src/components/InfoMessage.vue
+++ src/components/InfoMessage.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="w3-container w3-card-4" style="min-width:300px; max-width:300px; display:inline-block;">
-    <transition name="slide-fade">
-      <p style="text-align: center; vertical-align: middle; line-height: 30px" v-if="show"> {{mensaje}}</p>
-    </transition>
+  <div  id="myModal" class="modal w3-container " style="display:inline-block;" v-if="show">
+     
+    <div class="modal-content  w3-animate-top ">
+        <span class="close" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+        <h4 style="text-align: center; vertical-align: middle; margin-top:40px;"> {{mensaje}} </h4>
+    </div>
+
   </div>
+
 </template>
 
 <script>
@@ -30,17 +34,17 @@ export default {
   mounted: function() {
     EventBus.$on('showMessage', function(message) {
       this.showMessage(message);
-      setTimeout(this.hideMessage, 1500)
+      
     }.bind(this));
   }
 
-}
+};
 
 </script>
 
 <style>
 
-.slide-fade-enter-active {
+/*.slide-fade-enter-active {
   transition: all .3s ease;
 }
 .slide-fade-leave-active {
@@ -50,5 +54,49 @@ export default {
 {
   transform: translateX(10px);
   opacity: 0;
+}*/
+
+
+
+
+
+.modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    padding-top: 100px; /* Location of the box */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
 }
+
+/* Modal Content */
+.modal-content {
+    background-color: #fefefe;
+    margin: auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+}
+
+/* The Close Button */
+.close {
+    color: #aaaaaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+    color: #000;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+
 </style>
